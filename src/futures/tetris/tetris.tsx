2371b472f2c4d922c9adb105d18d2825f73ae3cd
yoshikouki <yoshikouki@gmail.com>
2024-05-27 11:13:06 +0900

    Boad are now responsive design compliant

--- src/futures/tetris/tetris.tsx
+++ src/futures/tetris/tetris.tsx
@@ -5,53 +5,50 @@ import { cn } from "@/lib/utils";
 import { useTetris } from "./use-tetris";
 
 export const Tetris = () => {
-  const { board, activeTetromino, startTetris } = useTetris();
+  const { board, activeTetromino, startTetris, isActiveTetromino } =
+    useTetris();
 
   return (
-    <div className="flex max-h-screen items-center justify-center">
-      <div className="relative border-2 bg-gray-100">
-        {board.rows.map((row) => (
-          <div key={row.id} className="flex">
-            {row.cells.map((cell) => (
+    <div className="relative w-full max-w-sm p-4">
+      <div
+        className="grid w-full"
+        style={{
+          gridTemplateRows: `repeat(${board.rowsNumber}, 1fr)`,
+          gridTemplateColumns: `repeat(${board.colsNumber}, 1fr)`,
+        }}
+      >
+        {board.rows.map((row, rowIndex) =>
+          row.cells.map((cell, cellIndex) =>
+            isActiveTetromino(cellIndex, rowIndex) ? (
+              <div
+                key={`${activeTetromino?.id}-${rowIndex}-${cellIndex}`}
+                className="aspect-square border-4 bg-primary opacity-90"
+              />
+            ) : (
               <div
                 key={cell.id}
                 className={cn(
-                  "h-10 w-10 border-2",
+                  "aspect-square border-2",
                   cell.tetrominoId && "bg-primary",
                 )}
               />
-            ))}
-          </div>
-        ))}
-        {activeTetromino?.shape.map((row, y) => (
-          <div key={`${activeTetromino.id}-${y}`} className="flex">
-            {row.map((cell, x) => (
-              <div
-                key={`${activeTetromino.id}-${y}-${x}`}
-                className="absolute h-10 w-10 border-4 bg-primary opacity-90"
-                style={{
-                  top: (activeTetromino.position.y + y) * 40,
-                  left: (activeTetromino.position.x + x) * 40,
-                }}
-                hidden={!cell}
-              />
-            ))}
-          </div>
-        ))}
-        <div
-          className={cn(
-            "absolute inset-0 flex w-full items-center justify-center",
-            board.status !== "ready" && "hidden",
-          )}
+            ),
+          ),
+        )}
+      </div>
+      <div
+        className={cn(
+          "absolute inset-0 flex w-full items-center justify-center",
+          board.status !== "ready" && "hidden",
+        )}
+      >
+        <Button
+          type="button"
+          onClick={startTetris}
+          className="p-12 font-black text-6xl"
         >
-          <Button
-            type="button"
-            onClick={startTetris}
-            className="p-12 font-black text-6xl"
-          >
-            Start
-          </Button>
-        </div>
+          Start
+        </Button>
       </div>
     </div>
   );
