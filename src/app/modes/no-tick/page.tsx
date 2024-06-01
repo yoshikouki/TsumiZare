a210a8dec0c8401482b69666098556426bced653
yoshikouki <yoshikouki@gmail.com>
2024-06-01 23:03:35 +0900

    Create no-tick mode page

--- src/app/modes/no-tick/page.tsx
+++ src/app/modes/no-tick/page.tsx
@@ -1,21 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { Header } from "./header";
-import Link from "next/link";
+import { Header } from "@/app/header";
 import { TsumiZare } from "@/futures/tsumizare/tsumizare";
 import { TsumiZareProvider } from "@/futures/tsumizare/tsumizare-provider";
 
-export default async function HomePage() {
+export default function NoTickModePage() {
   return (
     <TsumiZareProvider>
       <Header />
       <main className="flex min-h-svh flex-col items-center">
         <TsumiZare />
-
-        <div>
-          <Button asChild variant="outline">
-            <Link href="/modes/no-tick">Freeze Fun Mode</Link>
-          </Button>
-        </div>
       </main>
     </TsumiZareProvider>
   );
