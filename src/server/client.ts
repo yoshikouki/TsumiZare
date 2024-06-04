b15e7edbcaa27fb6d02d1cfffa70397c75acd44d
yoshikouki <yoshikouki@gmail.com>
2024-06-04 22:16:40 +0900

    Fix build for Cloudflare

--- src/server/client.ts
+++
@@ -1,7 +0,0 @@
-import { hc } from "hono/client";
-import type { AppType } from ".";
-
-const apiUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:8888/api";
-
-export const client = hc<AppType>(apiUrl);
-// type ClientRequest = HonoClientRequest<AppType>;
