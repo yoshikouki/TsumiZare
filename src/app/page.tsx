e7199baef74dbc11d029de0c1ea70001f8d60ee0
yoshikouki <yoshikouki@gmail.com>
2024-06-04 09:05:00 +0900

    Change modes navigation to Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import { Button } from "@/components/ui/button";
+import { BlockyGameProvider } from "blocky-game";
+import Link from "next/link";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import {
   TypographyH2,
-  TypographyH3,
   TypographyLead,
   TypographyP,
 } from "@/components/ui/typography";
 import { TsumiZare } from "@/futures/tsumizare/tsumizare";
-import { BlockyGameProvider } from "blocky-game";
-import Link from "next/link";
 import { Header } from "./header";
 
 export default async function HomePage() {
@@ -17,7 +23,7 @@ export default async function HomePage() {
       <main className="flex flex-col items-center gap-4">
         <TsumiZare />
 
-        <section id="about" className="max-w-xs space-y-4 py-4">
+        <section id="about" className="max-w-xs space-y-4 pt-20 pb-32">
           <TypographyH2 className="sr-only">TsumiZare とは</TypographyH2>
           <TypographyLead>
             TsumiZare "積戯" は、
@@ -25,16 +31,24 @@ export default async function HomePage() {
             テトリスのようなブロック落としゲームです。
           </TypographyLead>
         </section>
-        <section id="game-modes" className="max-w-xs space-y-4 py-4">
+
+        <section id="game-modes" className="flex max-w-xs flex-col gap-4 py-4">
           <TypographyH2>ゲームモード</TypographyH2>
-          <TypographyH3>幼児向け</TypographyH3>
-          <Button asChild variant="outline" className="">
-            <Link href="/modes/no-tick">Freeze Fun Mode</Link>
-          </Button>
-          <TypographyP>
-            幼児向けに設計されたモードです。ブロックが自動で落下せず、自由に操作できます。積み木遊びのように楽しめます。
-          </TypographyP>
+          <Link href="/modes/no-tick">
+            <Card className="py-4">
+              <CardHeader>
+                <CardTitle>つみき</CardTitle>
+                <CardDescription>
+                  積み木遊びのように楽しめる、子ども向けのモードです。
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                ブロックが自動で落下せず、自由に操作できます。
+              </CardContent>
+            </Card>
+          </Link>
         </section>
+
         <section id="how-to-play" className="max-w-xs space-y-4 py-4">
           <TypographyH2>プレイ方法</TypographyH2>
           <h3 className="font-semibold text-xl">スマホ</h3>
